e471b94d0819d3ab1a724c0d6cb92dfa319899eb
WH <weldon23.henson23@gmail.com>
2014-07-01 10:04:40 -0400

    New Variable Names
    ArcherDrupal needs to be Unique!

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 /**
  * @file
- * The PHP page that serves all page requests on a Drupal installation.
+ * The PHP page that serves all page requests on a Drupal and ArcherDrupal installation.
  *
  * The routines here dispatch control to the appropriate handler, which then
  * prints the appropriate page.
@@ -14,7 +14,7 @@
 /**
  * Root directory of Drupal installation.
  */
-define('DRUPAL_ROOT', getcwd());
+define('ARCHERDRUPAL_ROOT', getcwd());
 
 require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
