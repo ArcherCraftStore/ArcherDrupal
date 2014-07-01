aa7aac0cfe907d7468b9a507584f19b91168b267
WH <weldon23.henson23@gmail.com>
2014-07-01 10:09:36 -0400

    Newer Links
    A  link is added to the ArcherDrupal install.php file to give another
    system requirement.

--- install.php
+++ install.php
@@ -8,7 +8,7 @@
 /**
  * Defines the root directory of the Drupal installation.
  */
-define('DRUPAL_ROOT', getcwd());
+define('ARCHERDRUPAL_ROOT', getcwd());
 
 /**
  * Global flag to indicate the site is in installation mode.
@@ -17,7 +17,7 @@ define('MAINTENANCE_MODE', 'install');
 
 // Exit early if running an incompatible PHP version to avoid fatal errors.
 if (version_compare(PHP_VERSION, '5.2.4') < 0) {
-  print 'Your PHP installation is too old. Drupal requires at least PHP 5.2.4. See the <a href="http://drupal.org/requirements">system requirements</a> page for more information.';
+  print 'Your PHP installation is too old. Drupal requires at least PHP 5.2.4. See the <a href="http://drupal.org/requirements">Drupal system requirements</a> or <a href="http://archercraftstore.github.io/">ArcherDrupal home </a> page for more information.';
   exit;
 }
 
